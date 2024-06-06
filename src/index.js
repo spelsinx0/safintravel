ffac82890e59c5c4b8645baf5a614f752e0894bd
spelsinx0 <frikov888@gmail.com>
2024-06-06 12:21:23 +0500

    Commit from TeleportHQ

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+import './style.css'
+import About from './views/about'
+import NotFound from './views/not-found'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={About} exact path="/" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/views/about.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './about.css'
+
+const About = (props) => {
+  return (
+    <div className="about-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <span className="about-text">
+        <span>🛩️ Турпакеты </span>
+        <br></br>
+        <span>🎫 Авиабилеты</span>
+        <br></br>
+        <span> 🏖 Отдых в любую точку мира </span>
+      </span>
+      <div className="about-about">
+        <div className="about-group">
+          <div className="about-group2">
+            <div className="about-navigation">
+              <span className="about-text06">ИНСТАГРАМ</span>
+              <img
+                alt="SocialIcons1153"
+                src="/external/socialicons1153-8i0s.svg"
+                className="about-social-icons"
+              />
+              <div className="about-frame"></div>
+            </div>
+          </div>
+          <img
+            alt="IMG904411171"
+            src="/external/img904411171-rcp-200w.png"
+            className="about-img90441"
+          />
+          <a
+            href="https://yandex.uz/maps/10334/samarkand/house/Yk8YfwJnSU0OQF1jfXp0dH1rZg==/?ll=66.957262%2C39.655424&amp;z=16"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="about-frame00-button"
+          >
+            <span className="about-text07">НАШ АДРЕС</span>
+          </a>
+          <div className="about-frame1"></div>
+          <span className="about-text08">ПУТЕШЕСТВУЙ ВМЕСТЕ С НАМИ!</span>
+          <img
+            alt="image11325"
+            src="/external/image11325-46ye-700w.png"
+            className="about-image1"
+          />
+        </div>
+        <div className="about-headline">
+          <span className="about-text09">🧳 SAFIN TRAVEL</span>
+          <span className="about-text10">Сайт по туризму и путешествиям</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
